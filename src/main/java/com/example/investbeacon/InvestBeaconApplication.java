bfdbf8e1cf1f6dfb8695400fd1c2d5d556b423d7
Jonathan Robles <jonathan.robles287@gmail.com>
2022-03-07 16:31:22 -0600

    first commit

---
+++ src/main/java/com/example/investbeacon/InvestBeaconApplication.java
@@ -0,0 +1,13 @@
+package com.example.investbeacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestBeaconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestBeaconApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/investbeacon/InvestBeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investbeacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestBeaconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
