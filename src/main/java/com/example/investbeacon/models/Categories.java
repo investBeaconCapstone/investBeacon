1df9d7dba0835e52592ea8852764a5cbfd396303
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-08 09:21:38 -0600

    added stuff after first merge

---
+++ src/main/java/com/example/investbeacon/models/Categories.java
@@ -0,0 +1,56 @@
+package com.example.investbeacon.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="categories")
+public class Categories {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Categories> books;
+
+    public Genre() {
+    }
+
+    public Genre(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public Genre(long id, String name, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
