7b988d067f67f7ec14665147e94c611c1cc7ef04
Jonathan Robles <jonathan.robles287@gmail.com>
2022-03-08 10:51:58 -0600

    Created followers table and cleaned up getters and setters.

--- src/main/java/com/example/investbeacon/models/User.java
+++ src/main/java/com/example/investbeacon/models/User.java
@@ -1,10 +1,15 @@
 package com.example.investbeacon.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.swing.*;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
     @Id
@@ -35,7 +40,26 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<ForumPost> forumPosts;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "followers",
+            joinColumns = {@JoinColumn (name = "user_id")},
+            inverseJoinColumns ={@JoinColumn(name = "follow_id")})
+    private List<User> users;
+
+
     public User() {}
+
+    public User(Boolean isAdmin, String username, String first_name, String last_name, String email, String password, ImageIcon profile_img, List<ForumPost> forumPosts, List<User> users) {
+        this.isAdmin = isAdmin;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+        this.profile_img = profile_img;
+        this.forumPosts = forumPosts;
+        this.users = users;
+    }
 
     public User(long id, Boolean isAdmin, String username, String first_name, String last_name, String email, String password, ImageIcon profile_img, List<ForumPost> forumPosts) {
         this.id = id;
@@ -49,77 +73,4 @@ public class User {
         this.forumPosts = forumPosts;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Boolean getAdmin() {
-        return isAdmin;
-    }
-
-    public void setAdmin(Boolean admin) {
-        isAdmin = admin;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ImageIcon getProfile_img() {
-        return profile_img;
-    }
-
-    public void setProfile_img(ImageIcon profile_img) {
-        this.profile_img = profile_img;
-    }
-
-
-    public List<ForumPost> getForumPosts() {
-        return forumPosts;
-    }
-
-    public void setForum_posts(List<ForumPost> forumPosts) {
-        this.forumPosts = forumPosts;
-
-    }
 }
