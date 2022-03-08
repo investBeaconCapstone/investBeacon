bbf99a4c3f3f2bb29f750b4681c67885000fa085
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-08 06:23:41 -0600

    comments JPA

---
+++ src/main/java/com/example/investbeacon/models/Comments.java
@@ -0,0 +1,37 @@
+package com.example.investbeacon.models;
+
+
+import org.apache.catalina.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "comments")
+public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Lob
+    @Column(name="content", length=512)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
