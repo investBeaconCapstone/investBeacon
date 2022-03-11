e14f3346acf29517939d171f4a6c0ec9ce224ecb
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-11 09:34:47 -0600

    changes index page

--- src/main/java/com/example/investbeacon/models/ForumPostLike.java
+++ src/main/java/com/example/investbeacon/models/ForumPostLike.java
@@ -1,12 +1,12 @@
-package com.example.investbeacon.models;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "forum_posts_likes")
-public class ForumPostLike {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Byte like;
-
-}
+//package com.example.investbeacon.models;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "forum_posts_likes")
+//public class ForumPostLike {
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    private Byte like;
+//
+//}
--- src/main/java/com/example/investbeacon/repositories/LikeRepository.java
+++ src/main/java/com/example/investbeacon/repositories/LikeRepository.java
@@ -1,9 +1,9 @@
-package com.example.investbeacon.repositories;
-
-import com.example.investbeacon.models.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository <Like, Long>{
-
-
-}
+//package com.example.investbeacon.repositories;
+//
+//import com.example.investbeacon.models.Like;
+//import org.springframework.data.jpa.repository.JpaRepository;
+////
+////public interface LikeRepository extends JpaRepository <Like, Long>{
+////
+////
+////}
