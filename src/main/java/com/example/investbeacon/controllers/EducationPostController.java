f9e8666b473b2cfbde4ccdd085c2f3ca172cad11
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-03-11 12:11:31 -0600

    committing for merge

--- src/main/java/com/example/investbeacon/controllers/EducationPostController.java
+++ src/main/java/com/example/investbeacon/controllers/EducationPostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -41,6 +42,7 @@ public class EducationPostController {
 
     @GetMapping("/education/posts/create")
     public String viewCreate(Model model) {
+
         model.addAttribute("post", new EducationPost());
         model.addAttribute("cat", catDao.findAll());
 
@@ -53,7 +55,6 @@ public class EducationPostController {
         post.setUser(user);
 
         post.setCreatedDate(new Date());
-        System.out.println(post.getCategory().getCategory());
         postDao.save(post);
 
         switch (post.getCategory().getCategory()){
@@ -76,20 +77,24 @@ public class EducationPostController {
         EducationPost editPost = postDao.getById(id);
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (editPost.getUser().getId() == loggedInUser.getId()) {
-
+            model.addAttribute("cat", catDao.findAll());
             model.addAttribute("post", editPost);
             return "/education/edit";
         } else {
-            return "redirect:/";
+            return "redirect:/login";
         }
     }
 
     @PostMapping("/education/posts/{id}/edit")
     public String postEdit(@PathVariable long id, @ModelAttribute EducationPost post) {
 
+
+
         if (postDao.getById(id).getUser().getId() == (((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId())) {
             post.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+            post.setCreatedDate(new Date());
             postDao.save(post);
+
         }
         switch (post.getCategory().getCategory()){
             case "Crypto":
