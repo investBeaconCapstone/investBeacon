1df7e3ad9a72f5e673908592174c5209a2fca966
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-15 10:22:42 -0500

    changes on navbar added api

---
+++ keys.js
@@ -0,0 +1,6 @@
+
+//marketaux API
+
+const MARKETAUX_API="ZaTmr6mrrox3LxBsnqE92JhHuk6bgjAYWUVgoyFR";
+
+
--- market-tickers.js
+++ market-tickers.js
@@ -1,2 +1,55 @@
 
+"use strict";
 
+
+$(document).ready(function () {
+
+    //
+    // //  url for API
+    // const API_URL = 'https://api.marketaux.com/v1/news/all' + MARKETAUX_API;
+    //
+    // /** VARIABLES */
+    // let market = $('#load-market')
+    //
+    //
+    // $('#main').load("body", function () {
+    //     $('#load-market'), function () {
+    //
+    //         getMarketTickers();// added function
+    //
+    //
+    //         console.log("something happened");
+    //     }
+    // });
+    //
+    // let getMarketTickers = () => {
+    //     return fetch(MARKETAUX_API)
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             // console.log(data);
+    //             let output = " ";
+    //             for (let property of data) {
+    //                 // console.log(`Movie List:\nTitle: ${property.title}\nActors:${property.actors}\nYear: ${property.year}`);
+    //                 // added div class cards/img property and buttons
+    //                 $('#load-market').append(`<div class="col-md-3 my-col cards card-inf2" >
+    //                 </div>
+    //                 </div>`)
+    //             }
+    //
+    //         })
+    //
+    //
+    //
+    //
+    // };
+
+
+
+
+
+
+
+
+
+
+})
