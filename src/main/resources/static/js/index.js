c3645d20589c0591c093b44facf1f8a56801becb
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-16 18:43:35 -0500

    added api

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,103 +1,124 @@
 
 
-    //  url for API
-    const NEWS_API = ' https://api.marketaux.com/v1/news/all?symbols=TSLA,AMZN,MSFT&filter_entities=true&language=en&api_token=ZaTmr6mrrox3LxBsnqE92JhHuk6bgjAYWUVgoyFR' ;
 
 
-    const MARKET_API = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=WF0J3C09DVQXVCSK'
+    // const MARKET_API = 'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
+    const MARKET_API =  'https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
+    const CRYPTO_API = 'https://api.polygon.io/v1/open-close/crypto/BTC/USD/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
+    // const NEWS_API = ' https://api.marketaux.com/v1/news/all?symbols=TSLA,AMZN,MSFT&filter_entities=true&language=en&api_token=' + MARKETAUX_API;
 
-    // const CRYPTO_API = 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=5min&apikey=WF0J3C09DVQXVCSK'
-    const CRYPTO_API = 'https://api.polygon.io/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=10&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
 
 
+    //MARKET//
+     fetch(MARKET_API)
+        .then(data => {
+            return data.json();
+        })
+        .then(data => {
+            // console.log(data);
+        });
+
+    let getStocks = () => {
+        return fetch(MARKET_API)
+            .then(resp => resp.json())
+            .then(data => {
+                // console.log(data);
+                $('#load-market').empty();
+                let market = data.results;
+
+                for (let result of market) {
+                    //cards
+                    $('#load-market').append(`
+                      <li  style="list-style-type: none; color: white">
+                     <span class="mx-3"> ${result.T}</span>
+                     <i class="fa-solid fa-arrow-up" style="color: #2EB82E"></i><span class="mx-2" style="color: white">${result.h} </span>
+                     <i class="fa-solid fa-arrow-down-long" style="color: red"></i><span class="mx-2" style="color: white">${result.l}</span>
+                     </li>`)
+                }
+            })
+    }
+    getStocks();
+
+
+
+
+   // CRYPTO//
     fetch(CRYPTO_API)
         .then(data => {
             return data.json();
         })
         .then(data => {
-            console.log(data);
+            // console.log(data);
         });
 
-
-
-/// market news// working on test page not working here
-
-
-
-    fetch(NEWS_API)
-        .then(data => {
-            return data.json();
-        })
-        .then(data => {
-            console.log(data);
-        });
-
-    // market news//
-
-    let getMarketNews = () => {
-        return fetch(NEWS_API)
+    let getCrypto = () => {
+        return fetch(CRYPTO_API)
             .then(resp => resp.json())
             .then(data => {
-                console.log(data);
-                $('#load-news').empty();
-                let newsMarket = data.data;
+                // console.log(data);
+                $('#load-crypto').empty();
+                let crypto = data;
 
-                for (let property of newsMarket) {
-                    //cards
-                    $('#load-news').append(`<div>
-                    <div class="card" style="width: 30rem;">
-                        <h3 class="newsTitle text-center">${property.title.toUpperCase()}</h3>
-                        <img id="newsPoster" src="${property.image_url}" class="card-img-top" alt="pic" style="width: 20rem;">
-                        <div class="card-body body">
-                        <p class="newsDescription">${property.description}</p>
-                       
-                       <a class="newsUrl">${property.url}</a>
-                 </div>
-                 </div>`)
-                }
+                // for (let result of crypto) {
+                //     //cards
+                    $('#load-crypto').append(`
+                      <li>
+                     <span> ${crypto.symbol}</span>
+                     </li>
+                     <span>${crypto.open} </span>
+                     <span>${crypto.close}</span>
+                      `)
+                 // }
             })
     }
 
-    getMarketNews();
+    getCrypto();
 
 
-    // CRYPTO//
 
-    // fetch(CRYPTO_API)
+   //MARKET NEWS//
+
+
+
+
+    // fetch(NEWS_API)
     //     .then(data => {
     //         return data.json();
     //     })
     //     .then(data => {
     //         console.log(data);
     //     });
+    //
+    // // market news//
+    //
+    // let getMarketNews = () => {
+    //     return fetch(NEWS_API)
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             $('#load-news').empty();
+    //             let newsMarket = data.data;
+    //
+    //             for (let property of newsMarket) {
+    //                 //cards
+    //                 $('#load-news').append(`<div>
+    //                 <div class="card" style="width: 30rem;">
+    //                     <h3 class="newsTitle text-center">${property.title.toUpperCase()}</h3>
+    //                     <img id="newsPoster" src="${property.image_url}" class="card-img-top" alt="pic" style="width: 20rem;">
+    //                     <div class="card-body body">
+    //                     <p class="newsDescription">${property.description}</p>
+    //
+    //                    <a class="newsUrl">${property.url}</a>
+    //              </div>
+    //              </div>`)
+    //             }
+    //         })
+    // }
+    //
+    // getMarketNews();
 
 
 
-    let getCrypto = () => {
-        return fetch(CRYPTO_API)
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data);
-                $('#load-market').empty();
-                let market = data.results;
-
-                for (let result of market) {
-                    //cards
-                    $('#load-market').append(`<div class="row" >
-                    <div class="card" style="width: 15rem;">
-                        <p class="newsTitle text-center">${result.ticker}</p>
-                        <p class="newsDescription">${result.name}</p>
-                        <p class="newsDescription">${result.primary_exchange}</p>
-                        <p class="newsDescription">${result.type}</p>
-                        <p class="newsDescription">${result.locale}</p>
-                       
-                     </div>
-                 </div>`)
-                }
-            })
-    }
-
-    getCrypto();
 
 
 
