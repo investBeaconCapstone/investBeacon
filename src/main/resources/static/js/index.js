a0ee3b111f28d67ac1517624f74840276de20a7c
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-03-16 21:07:27 -0500

    added js for news market and crypto

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,12 +1,15 @@
 
 
 
-
+//https://eodhistoricaldata.com/api/real-time/BTC-USD.CC?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX&fmt=json&s=ETH,LTC,ADA,DOT,BCH,XLM,DOGE,BNB,USDT,XMR
     // const MARKET_API = 'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
-    const MARKET_API =  'https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
-    const CRYPTO_API = 'https://api.polygon.io/v1/open-close/crypto/BTC/USD/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
-    // const NEWS_API = ' https://api.marketaux.com/v1/news/all?symbols=TSLA,AMZN,MSFT&filter_entities=true&language=en&api_token=' + MARKETAUX_API;
+    // const MARKET_API =  'https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
 
+
+    const CRYPTO_API = 'https://api.polygon.io/v1/open-close/crypto/BTC/USD/2020-10-14?adjusted=true&apiKey=gHpHxDBVFlIGHibiBioo052yi8honmWu';
+    const NEWS_API = ' https://eodhistoricaldata.com/api/news?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX&s=AAPL.US&offset=0&limit=3';
+
+    const MARKET_API = ' https://eodhistoricaldata.com/api/real-time/AAPL.US?api_token=62323211e2b454.72150400&fmt=json&s=VTI,EUR.FOREX,MSFT,TSLA,UNH,GOOGL,AMZN,FB,V,UNH';
 
 
     //MARKET//
@@ -15,7 +18,7 @@
             return data.json();
         })
         .then(data => {
-            // console.log(data);
+            console.log(data);
         });
 
     let getStocks = () => {
@@ -24,15 +27,15 @@
             .then(data => {
                 // console.log(data);
                 $('#load-market').empty();
-                let market = data.results;
+                let market = data;
 
                 for (let result of market) {
                     //cards
                     $('#load-market').append(`
-                      <li  style="list-style-type: none; color: white">
-                     <span class="mx-3"> ${result.T}</span>
-                     <i class="fa-solid fa-arrow-up" style="color: #2EB82E"></i><span class="mx-2" style="color: white">${result.h} </span>
-                     <i class="fa-solid fa-arrow-down-long" style="color: red"></i><span class="mx-2" style="color: white">${result.l}</span>
+                      <li  style="list-style-type: none; color: white; display:inline-block;">
+                     <span class="mx-3"> ${result.code}</span>
+                     <i class="fa-solid fa-arrow-up" style="color: #2EB82E"></i><span class="mx-2" style="color: white">${result.high} </span>
+                     <i class="fa-solid fa-arrow-down-long" style="color: red"></i><span class="mx-2" style="color: white">${result.low}</span>
                      </li>`)
                 }
             })
@@ -79,43 +82,39 @@
    //MARKET NEWS//
 
 
+    fetch(NEWS_API)
+        .then(data => {
+            return data.json();
+        })
+        .then(data => {
+            console.log(data);
+        });
 
+    // market news//
 
-    // fetch(NEWS_API)
-    //     .then(data => {
-    //         return data.json();
-    //     })
-    //     .then(data => {
-    //         console.log(data);
-    //     });
-    //
-    // // market news//
-    //
-    // let getMarketNews = () => {
-    //     return fetch(NEWS_API)
-    //         .then(resp => resp.json())
-    //         .then(data => {
-    //             console.log(data);
-    //             $('#load-news').empty();
-    //             let newsMarket = data.data;
-    //
-    //             for (let property of newsMarket) {
-    //                 //cards
-    //                 $('#load-news').append(`<div>
-    //                 <div class="card" style="width: 30rem;">
-    //                     <h3 class="newsTitle text-center">${property.title.toUpperCase()}</h3>
-    //                     <img id="newsPoster" src="${property.image_url}" class="card-img-top" alt="pic" style="width: 20rem;">
-    //                     <div class="card-body body">
-    //                     <p class="newsDescription">${property.description}</p>
-    //
-    //                    <a class="newsUrl">${property.url}</a>
-    //              </div>
-    //              </div>`)
-    //             }
-    //         })
-    // }
-    //
-    // getMarketNews();
+    let getMarketNews = () => {
+        return fetch(NEWS_API)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                $('#load-news').empty();
+                let newsMarket = data;
+
+                for (let property of newsMarket) {
+                    //cards
+                    $('#load-news').append(`
+                    <div class="card" style="width: 30rem;">
+                        <h3 class="newsTitle text-center">${property.title.toUpperCase()}</h3>
+                        
+                        <p class="newsDescription">${property.content}</p>
+
+                       <a class="newsUrl">${property.link}</a>
+                 </div>`)
+                }
+            })
+    }
+
+    getMarketNews();
 
 
 
