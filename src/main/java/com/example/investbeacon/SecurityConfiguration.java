25be647bf2d0c0f1ed2cc207f3a7aec08acf0c7d
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-03-21 14:35:46 -0500

    updated security configuration

--- src/main/java/com/example/investbeacon/SecurityConfiguration.java
+++ src/main/java/com/example/investbeacon/SecurityConfiguration.java
@@ -45,15 +45,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/register", "/profile{id}") // anyone can see the home and the register pages
+                .antMatchers("/", "/register", "/profile/{id}","/forum-posts","/forum-posts/{id}", "/education/posts/{category}") // anyone can see the home and the register pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
                 .authorizeRequests()
                 .antMatchers(
-                        "/posts/create", // only authenticated users can create posts
-                        "/posts/{id}/edit", // only authenticated users can edit posts
+                        "/forum-posts/create", // only authenticated users can create posts
+                        "/forum-posts/{id}/edit", // only authenticated users can edit posts
+                        "/forum-posts/{id}/delete", // only authenticated users can delete posts
+                        "/forum-posts/{id}/comment/{commentId}/edit", //only authenticated users can edit comments
+                        "/forum-posts/{id}/comment/{commentId}/delete", //only authenticated users can delete comments
+                        "/forum-posts/{id}/like-unlike", //only authenticated users can vote
                         "/profile/{id}/edit" // only authenticated users can edit profile
+
+
                 )
                 .authenticated()
         ;
