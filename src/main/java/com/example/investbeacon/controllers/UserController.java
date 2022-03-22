b62fc373f2ade4e8313b12f738751823f39cd873
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-03-22 16:05:50 -0500

    change pw functionality

--- src/main/java/com/example/investbeacon/controllers/UserController.java
+++ src/main/java/com/example/investbeacon/controllers/UserController.java
@@ -183,11 +183,10 @@ public class UserController {
     @PostMapping("/password/{id}/change")
     public String changePassword(@RequestParam("oldPassword") String oldPassword, @RequestParam("newPassword") String newPassword, @ModelAttribute User passwordEdit, @PathVariable long id) {
         if (userDao.getById(id).getId() == ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId()) {
-            if(passwordEncoder.matches(oldPassword, passwordEdit.getPassword())) {
+            if(passwordEncoder.matches(oldPassword, userDao.getById(id).getPassword())) {
                 passwordEdit.setPassword(passwordEncoder.encode(newPassword));
                 String hash = passwordEncoder.encode(passwordEdit.getPassword());
                 passwordEdit.setPassword(hash);
-
             }
             userDao.save(passwordEdit);
             return "redirect:/profile/{id}";
