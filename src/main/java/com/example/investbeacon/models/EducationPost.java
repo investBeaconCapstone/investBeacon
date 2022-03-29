d25e66fc24d0bc17c72bfd4475c4128c6c31130e
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-03-29 11:03:00 -0400

    added validation to education create

--- src/main/java/com/example/investbeacon/models/EducationPost.java
+++ src/main/java/com/example/investbeacon/models/EducationPost.java
@@ -4,6 +4,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
@@ -15,11 +16,12 @@ public class EducationPost {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-
+    @NotBlank(message = "Please add a title")
+    @Size(min = 3, message = "The title must be at least 3 characters.")
     @Column(nullable = false, length = 100)
     private String title;
 
-
+    @NotBlank(message = "Please add content to the form")
     @Column(nullable = false,  length = 3500)
     private String description;
 
@@ -30,7 +32,7 @@ public class EducationPost {
     @Column(name ="content_img_url", length = 500)
     private String contentImageUrl;
 
-
+    @NotNull(message = "Please Select a Category")
     @ManyToOne
     @JoinColumn (name = "cat_id")
     private Category category;
